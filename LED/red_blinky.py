e158a9d27eb5a50da52344ed8cf8e7f616eb47e4
Eric Muntz <ericmuntz@gmail.com>
2015-07-14 21:34:42 +0000

    make the red light go blinky

---
+++ LED/red_blinky.py
@@ -0,0 +1,21 @@
+import time
+
+try:
+    import RPi.GPIO as GPIO
+except RuntimeError:
+    print("Error, you prolly need some extra permissions")
+
+pin = 12
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(pin, GPIO.OUT)
+GPIO.output(pin, True)
+time.sleep(2);
+GPIO.output(pin, False) 
+time.sleep(2);
+GPIO.output(pin, True) 
+time.sleep(2);
+GPIO.output(pin, False) 
+
+GPIO.cleanup()
+
